7aca11c7496f9cb1fbc7e2b779291d6cbc30aa98
Ilya Vassyutovich <vasutovich@mindbox.ru>
2020-05-09 20:30:59 +0300

    Use configuration

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 
 namespace ConsoleApplication
 {
@@ -7,7 +9,20 @@ namespace ConsoleApplication
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello, world!");
+
+			UseConfiguration();
+
 			Console.ReadLine();
+		}
+
+		private static void UseConfiguration()
+		{
+			var configuration = new ConfigurationBuilder()
+				.AddInMemoryCollection(
+					new[] {new KeyValuePair<string, string>("foo", "bar")})
+				.Build();
+
+			Console.WriteLine(configuration["foo"]);
 		}
 	}
 }
