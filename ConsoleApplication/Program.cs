98412fee844f48fda3ade930c1ffd8f18c2739d9
Ilya Vassyutovich <vasutovich@mindbox.ru>
2020-05-09 19:34:03 +0300

    Implement "hello world"

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+	internal static class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, world!");
+			Console.ReadLine();
+		}
+	}
+}
